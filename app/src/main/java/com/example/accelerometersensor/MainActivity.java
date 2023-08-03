4338ee85d0a12999d3d24d6d60aeb4ac928fb236
Basudev <samantabasu.2001@gmail.com>
2023-08-03 19:58:34 +0530

    Finished GravitySensor project of android.

---
+++ app/src/main/java/com/example/accelerometersensor/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.accelerometersensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    TextView textView;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Find id
+        textView = findViewById(R.id.textView);
+
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        if(sensorManager!=null){
+          Sensor accelerometerSensor =  sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+          if(accelerometerSensor!=null){
+            sensorManager.registerListener(this,accelerometerSensor,SensorManager.SENSOR_DELAY_NORMAL);
+          }
+        }else {
+            Toast.makeText(this, "Sensor service not detected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        textView.setText("X: "+sensorEvent.values[0]+" Y: "+sensorEvent.values[1]+" Z: "+sensorEvent.values[2]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
